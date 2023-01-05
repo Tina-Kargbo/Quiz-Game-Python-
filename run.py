41c187c273a4542223217a1de7251b129c0598c5
Tina-Kargbo <cl.amankwaah@gmail.com>
2023-01-05 13:11:54 +0000

    Determine if player wants to take quiz or not

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+QuizPlay = input("Hello there! Would you like to take the quiz? Yes or No? ")
+
+if QuizPlay == "Yes":
+    print("Great, Let's play!")
+elif QuizPlay == "No":
+    quit()
+else:
+    print("This input is not valid. Please answer Yes or No as a valid input.")
+
+
+#code was taken from python CI lesson, "What are input/output operations?"
+username = input("What is your name please?: ") 
+print("Hello " + username + "! You will have 5 questions to answer. Try to answer as many questions as you can correctly to test your level of knowledge on the subject matter.")
